9eaeb8728dded070f2d89027d9def3742152f3a8
70-codes <kinuthiasteve098@gmail.com>
2023-08-09 10:45:27 +0300

    created main file

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/home/creed347/anaconda3/envs/fastAPI/bin/python
+
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
